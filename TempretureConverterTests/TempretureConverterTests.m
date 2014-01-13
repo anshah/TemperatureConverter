cb617fa08e6c4c5fb312090fc194c44f43e976f6
anshah2 <ankitshah_1237@yahoo.com>
2014-01-13 00:02:49 -0800

    Initial Commit

---
+++ TempretureConverterTests/TempretureConverterTests.m
@@ -0,0 +1,34 @@
+//
+//  TempretureConverterTests.m
+//  TempretureConverterTests
+//
+//  Created by Ankit Nitin Shah on 1/13/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TempretureConverterTests : XCTestCase
+
+@end
+
+@implementation TempretureConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
